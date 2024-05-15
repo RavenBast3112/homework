730ee6bb0d9eb1b4df4c8a384df3b8ea1b43a6f9
user <blackraven3997@gmail.com>
2024-05-15 16:41:57 +0500

    new repo

---
+++ debounce/index.js
@@ -0,0 +1,8 @@
+function debounce(fn, delay) {
+  let timeout;
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => fn.apply(this, arguments), delay)
+  }
+};
+module.exports = debounce;
